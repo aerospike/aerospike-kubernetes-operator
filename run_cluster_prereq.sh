2cc9a4ebcb9f1d55c0a35d8394416cbcfb0cfdce
sud82 <sudhanshu.ranjan.82@gmail.com>
2020-10-15 02:01:10 -0700

    Added xdr_src, xdr_dst file using same operator

---
+++ run_cluster_prereq.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# cluster1
+kubectl create namespace aerospike
+sleep 2
+
+kubectl create secret generic aerospike-secret --from-file=deploy/secrets -n aerospike
+sleep 2
+
+kubectl create secret generic auth-secret --from-literal=password='admin123' -n aerospike
+sleep 2
+
+# cluster2
+kubectl create namespace aerospike1
+sleep 2
+
+kubectl create secret generic aerospike-secret --from-file=deploy/secrets -n aerospike1
+sleep 2
+
+kubectl create secret generic auth-secret --from-literal=password='admin123' -n aerospike1
+sleep 2
+
+# cluster3
+kubectl create namespace aerospike2
+sleep 2
+
+kubectl create secret generic aerospike-secret --from-file=deploy/secrets -n aerospike2
+sleep 2
+
+kubectl create secret generic auth-secret --from-literal=password='admin123' -n aerospike2
+sleep 2
+
+# rbac for all cluster
+kubectl apply -f deploy/rbac_cluster.yaml
+sleep 2
+
+# kubectl apply -f deploy/samples/hdd_dim_storage_cluster_cr.yaml
