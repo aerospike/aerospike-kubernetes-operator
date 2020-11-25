b9b4920805058e6535016c4ac0da5a8f40f6fcd1
Ashish Shinde <ashish@aerospike.com>
2020-11-25 22:30:04 +0530

    Ensured the custom resource is deployed

--- test/e2e/deploy-test-operator.sh
+++ test/e2e/deploy-test-operator.sh
@@ -13,6 +13,9 @@ if [[ ! -z "$IMAGE" ]]; then
     sed -i  "s@image: .*@image: $IMAGE@g" $DIR/setup_operator_test.yaml
 fi
 
+# Create the custom custom resource.
+kubectl -n test apply -f deploy/crds/aerospike.com_aerospikeclusters_crd.yaml
+
 # Create storage classes.
 kubectl -n test apply -f deploy/samples/storage-classes/local-storage-class.yaml
 kubectl -n test apply -f deploy/samples/storage-classes/gce-ssd-storage-class.yaml
