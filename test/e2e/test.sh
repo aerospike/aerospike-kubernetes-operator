80cd8457f7b51fdb790578963d5f670c445d3ad7
Ashish Shinde <ashish@aerospike.com>
2020-11-27 15:49:56 +0530

    Ensured clusters are deleted on failure

--- test/e2e/test.sh
+++ test/e2e/test.sh
@@ -7,7 +7,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Cleanup
-echo "Removing up operator residual k8s resources...."
+echo "Removing residual k8s resources...."
 $DIR/cleanup-test-namespace.sh
 
 # Setup the deploy-test-operator.sh
