459827408367e973daedb3f5dd11d441abe5e2ad
Ashish Shinde <ashish@aerospike.com>
2021-06-28 17:13:11 +0530

    Fixed access control tests

--- test/cleanup-test-namespace.sh
+++ test/cleanup-test-namespace.sh
@@ -10,18 +10,22 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Delete Aeropsike clusters
+echo "Removing Aerospike clusters"
 kubectl -n test delete aerospikecluster --all
 kubectl -n test1 delete aerospikecluster --all
 kubectl -n test2 delete aerospikecluster --all
 kubectl -n test3 delete aerospikecluster --all
 
 # Delete PVCs
+echo "Removing PVCs"
 kubectl -n test delete pvc --selector 'app=aerospike-cluster'
 
 # Delete the secrets
+echo "Removing secrets"
 kubectl -n test delete secret --selector 'app=aerospike-cluster' || true
 
 # Delete rbac accounts and auth
+echo "Removing RBAC"
 kubectl delete clusterrolebinding aerospike-cluster || true
 kubectl delete clusterrole aerospike-cluster || true
 kubectl -n test delete serviceaccount aerospike-cluster || true
@@ -29,9 +33,11 @@ kubectl -n test1 delete serviceaccount aerospike-cluster || true
 kubectl -n test2 delete serviceaccount aerospike-cluster || true
 
 # # Delete the operator deployment
+echo "Removing test operator deployment"
 make test-undeploy
 
 # Delete namespaces
+echo "Removing test namespaces"
 kubectl delete namespace test1 || true
 kubectl delete namespace test2 || true
 kubectl delete namespace test || true
