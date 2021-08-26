2e909db7ea29e3c49a0ffe77f671e83a77fcc31b
David Gerchikov <dgerchikov@aerospike.com>
2021-08-26 16:06:35 +0300

    Add schema loading

--- pkg/configschema/schemamap.go
+++ pkg/configschema/schemamap.go
@@ -1,78 +1,42 @@
 package configschema
 
-import _ "embed"
+import (
+	"embed"
+	_ "embed"
+	"io/fs"
+	"path/filepath"
+	"strings"
+)
 
-//go:embed jsonfiles/4_0_0.json
-var conf4_0_0 string
+//go:embed json
+var schemas embed.FS
 
-//go:embed jsonfiles/4_1_0.json
-var conf4_1_0 string
+type SchemaMap map[string]string
 
-//go:embed jsonfiles/4_2_0.json
-var conf4_2_0 string
+func NewSchemaMap() (SchemaMap, error) {
 
-//go:embed jsonfiles/4_3_0.json
-var conf4_3_0 string
+	schemamap := make(SchemaMap)
 
-//go:embed jsonfiles/4_3_1.json
-var conf4_3_1 string
+	if err := fs.WalkDir(schemas, ".", func(path string, d fs.DirEntry, err error) error {
 
-//go:embed jsonfiles/4_4_0.json
-var conf4_4_0 string
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			content, err := fs.ReadFile(schemas, path)
+			if err != nil {
+				return err
+			}
+			baseName := filepath.Base(path)
+			key := strings.TrimSuffix(baseName, filepath.Ext(baseName))
+			schemamap[key] = string(content)
+		}
+		return nil
 
-//go:embed jsonfiles/4_5_0.json
-var conf4_5_0 string
+	}); err != nil {
+		return nil, err
+	}
 
-//go:embed jsonfiles/4_5_1.json
-var conf4_5_1 string
+	return schemamap, nil
 
-//go:embed jsonfiles/4_5_2.json
-var conf4_5_2 string
-
-//go:embed jsonfiles/4_5_3.json
-var conf4_5_3 string
-
-//go:embed jsonfiles/4_6_0.json
-var conf4_6_0 string
-
-//go:embed jsonfiles/4_7_0.json
-var conf4_7_0 string
-
-//go:embed jsonfiles/4_7_0.json
-var conf4_8_0 string
-
-//go:embed jsonfiles/4_9_0.json
-var conf4_9_0 string
-
-//go:embed jsonfiles/5_0_0.json
-var conf5_0_0 string
-
-//go:embed jsonfiles/5_1_0.json
-var conf5_1_0 string
-
-//go:embed jsonfiles/5_2_0.json
-var conf5_2_0 string
-
-//go:embed jsonfiles/5_3_0.json
-var conf5_3_0 string
-
-//go:embed jsonfiles/5_4_0.json
-var conf5_4_0 string
-
-//go:embed jsonfiles/5_5_0.json
-var conf5_5_0 string
-
-//go:embed jsonfiles/5_6_0.json
-var conf5_6_0 string
-
-// SchemaMap has all supported json schema in string form
-var SchemaMap = map[string]string{
-	"4.9.0": conf4_9_0,
-	"5.0.0": conf5_0_0,
-	"5.1.0": conf5_1_0,
-	"5.2.0": conf5_2_0,
-	"5.3.0": conf5_3_0,
-	"5.4.0": conf5_4_0,
-	"5.5.0": conf5_5_0,
-	"5.6.0": conf5_6_0,
 }
