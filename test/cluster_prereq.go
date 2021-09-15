d60cd32c4327665bf3ce37a4a14eb0f7662eb487
sud82 <sudhanshu.ranjan.82@gmail.com>
2021-09-14 23:39:27 -0700

    Fix rbac for tests

--- test/cluster_prereq.go
+++ test/cluster_prereq.go
@@ -6,7 +6,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	rbac "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -26,23 +25,25 @@ func createClusterResource(k8sClient client.Client, ctx goctx.Context) error {
 		return err
 	}
 
-	operatorNs := namespace
-
 	// Create service account for getting access in cluster specific namespaces
-	if err := createServiceAccount(k8sClient, ctx, aeroClusterServiceAccountName, multiClusterNs1); err != nil {
+	if err := createServiceAccount(
+		k8sClient, ctx, aeroClusterServiceAccountName, multiClusterNs1,
+	); err != nil {
 		return err
 	}
-	if err := createServiceAccount(k8sClient, ctx, aeroClusterServiceAccountName, multiClusterNs2); err != nil {
+	if err := createServiceAccount(
+		k8sClient, ctx, aeroClusterServiceAccountName, multiClusterNs2,
+	); err != nil {
 		return err
 	}
-
+	// Create clusterRole for service accounts
+	if err := createClusterRole(
+		k8sClient, ctx, "aerospike-cluster",
+	); err != nil {
+		return err
+	}
 	// Create clusterRoleBinding to bind clusterRole and accounts
 	subjects := []rbac.Subject{
-		{
-			Kind:      "ServiceAccount",
-			Name:      aeroClusterServiceAccountName,
-			Namespace: operatorNs,
-		},
 		{
 			Kind:      "ServiceAccount",
 			Name:      aeroClusterServiceAccountName,
@@ -54,14 +55,48 @@ func createClusterResource(k8sClient client.Client, ctx goctx.Context) error {
 			Namespace: multiClusterNs2,
 		},
 	}
-	if err := createRoleBinding(k8sClient, ctx, aeroClusterRoleBindingName, subjects); err != nil {
+	if err := createRoleBinding(
+		k8sClient, ctx, aeroClusterRoleBindingName, subjects,
+		"aerospike-cluster",
+	); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func createNamespace(k8sClient client.Client, ctx goctx.Context, name string) error {
+func createClusterRole(
+	k8sClient client.Client,
+	ctx goctx.Context, name string,
+) error {
+	cr := &rbac.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Rules: []rbac.PolicyRule{
+			{
+				APIGroups: []string{""},
+				Resources: []string{"pods", "nodes", "services"},
+				Verbs:     []string{"get", "list"},
+			},
+			{
+				APIGroups: []string{"aerospike.com"},
+				Resources: []string{"*"},
+				Verbs:     []string{"*"},
+			},
+			{
+				APIGroups: []string{""},
+				Resources: []string{"pods"},
+				Verbs:     []string{"*"},
+			},
+		},
+	}
+	return k8sClient.Create(goctx.TODO(), cr)
+}
+
+func createNamespace(
+	k8sClient client.Client, ctx goctx.Context, name string,
+) error {
 	ns := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
@@ -70,7 +105,9 @@ func createNamespace(k8sClient client.Client, ctx goctx.Context, name string) er
 	return k8sClient.Create(ctx, ns)
 }
 
-func createServiceAccount(k8sClient client.Client, ctx goctx.Context, name string, namespace string) error {
+func createServiceAccount(
+	k8sClient client.Client, ctx goctx.Context, name string, namespace string,
+) error {
 	svcAct := &corev1.ServiceAccount{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
@@ -80,12 +117,20 @@ func createServiceAccount(k8sClient client.Client, ctx goctx.Context, name strin
 	return k8sClient.Create(ctx, svcAct)
 }
 
-func createRoleBinding(k8sClient client.Client, ctx goctx.Context, name string, subjects []rbac.Subject) error {
-	crb := rbac.ClusterRoleBinding{}
-	if err := k8sClient.Get(ctx, types.NamespacedName{Name: name}, &crb); err != nil {
-		return err
+func createRoleBinding(
+	k8sClient client.Client, ctx goctx.Context, name string,
+	subjects []rbac.Subject, roleRefName string,
+) error {
+	crb := &rbac.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Subjects: subjects,
+		RoleRef: rbac.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Name:     roleRefName,
+			Kind:     "ClusterRole",
+		},
 	}
-	crb.Subjects = subjects
-
-	return k8sClient.Update(ctx, &crb)
+	return k8sClient.Create(goctx.TODO(), crb)
 }
