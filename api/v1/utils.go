71443dbc59649ce3d11e002ec1fd6959c2afaa66
Abhisek Dwivedi <adwivedi@aerospike.com>
2024-01-10 17:28:17 +0530

    Fix vuls (#267)
    * Fix vulnerability
    
    * Bump aerospike-kubernetes-init version
    
    * Change management-lib version to tagged version
    
    * Update CRD manifests

--- api/v1/utils.go
+++ api/v1/utils.go
@@ -62,7 +62,7 @@ const (
 	AerospikeInitContainerRegistryEnvVar           = "AEROSPIKE_KUBERNETES_INIT_REGISTRY"
 	AerospikeInitContainerDefaultRegistry          = "docker.io"
 	AerospikeInitContainerDefaultRegistryNamespace = "aerospike"
-	AerospikeInitContainerDefaultRepoAndTag        = "aerospike-kubernetes-init:2.1.0"
+	AerospikeInitContainerDefaultRepoAndTag        = "aerospike-kubernetes-init:2.1.1"
 	AerospikeAppLabel                              = "app"
 	AerospikeCustomResourceLabel                   = "aerospike.com/cr"
 	AerospikeRackIDLabel                           = "aerospike.com/rack-id"
