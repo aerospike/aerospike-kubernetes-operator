ea609543710aba8b38182b7d0545d9e69c98111d
Tanmay Jain <tjain@aerospike.com>
2024-08-06 12:07:29 +0530

    Adding support for HPA

--- api/v1/aerospikecluster_types.go
+++ api/v1/aerospikecluster_types.go
@@ -722,7 +722,8 @@ type AerospikeClusterStatus struct { //nolint:govet // for readability
 	Pods map[string]AerospikePodStatus `json:"pods" patchStrategy:"strategic"`
 
 	// Phase denotes the current phase of Aerospike cluster operation.
-	Phase AerospikeClusterPhase `json:"phase,omitempty"`
+	Phase    AerospikeClusterPhase `json:"phase,omitempty"`
+	Selector string                `json:"selector"`
 }
 
 // AerospikeNetworkType specifies the type of network address to use.
@@ -921,6 +922,7 @@ type AerospikePodStatus struct { //nolint:govet // for readability
 // +kubebuilder:printcolumn:name="HostNetwork",type=boolean,JSONPath=`.spec.podSpec.hostNetwork`
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=".status.phase"
+//+kubebuilder:subresource:scale:specpath=.spec.size,statuspath=.status.size,selectorpath=.status.selector
 
 // AerospikeCluster is the schema for the AerospikeCluster API
 // +operator-sdk:csv:customresourcedefinitions:displayName="Aerospike Cluster",resources={{Service, v1},{Pod,v1},{StatefulSet,v1}}
