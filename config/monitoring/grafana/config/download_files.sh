9b0aa86b71e731513b65a11bd6530b45a3618b94
Tanmay Jain <tjain@aerospike.com>
2024-08-28 14:38:45 +0530

    Add support for downloading the latest dashboard version by allowing users to omit the revision number.

--- config/monitoring/grafana/config/download_files.sh
+++ config/monitoring/grafana/config/download_files.sh
@@ -1,30 +1,40 @@
 #!/bin/sh
 
-# dashboards to download
-# DASHBOARDS="<dashboard_id>:<revision> ..."
-DASHBOARDS="16119:10 16115:7"
+# Check if curl and jq is installed; if not, install curl and jq
+if ! command -v curl >/dev/null 2>&1 || ! command -v jq >/dev/null 2>&1; then
+    echo "curl or jq not found. Installing..."
+    apk add --no-cache curl jq
+else
+    echo "curl and jq are already installed."
+fi
 
+# Define the dashboards to download in the format <dashboard_id>:<revision> or <dashboard_id>
+DASHBOARDS="16119:10 16115:7 20279"
+
+# Directory where the dashboards will be saved
 TARGET_DIR="/mnt/data"
 mkdir -p "$TARGET_DIR"
 
+DELIMITER=':'
+
+# Loop through each dashboard identifier in DASHBOARDS
 for DASHBOARD in $DASHBOARDS; do
-  delimiter=':'
-
-  # Count the number of occurrences of the delimiter
-  count=$(echo "$DASHBOARD" | awk -F"$delimiter" '{print NF-1}')
-
-  if [ "$count" -eq 1 ]; then
-      # Split the string into two parts
-      ID=$(echo "$DASHBOARD" | cut -d"$delimiter" -f1)
-      REVISION=$(echo "$DASHBOARD" | cut -d"$delimiter" -f2)
-
-      URL=https://grafana.com/api/dashboards/$ID/revisions/$REVISION/download
-      FILENAME=$ID-"rev"$REVISION.json
-      curl -o "$TARGET_DIR/$FILENAME" "$URL"
+  if echo "$DASHBOARD" | grep -q "$DELIMITER"; then
+    # If the delimiter ':' exists, split into ID and REVISION
+    ID=$(echo "$DASHBOARD" | cut -d"$DELIMITER" -f1)
+    REVISION=$(echo "$DASHBOARD" | cut -d"$DELIMITER" -f2)
+    FILENAME="$ID-rev$REVISION.json"
+    URL="https://grafana.com/api/dashboards/$ID/revisions/$REVISION/download"
+    curl -o "$TARGET_DIR/$FILENAME" "$URL"
   else
-      echo "Error: The string must contain exactly one delimiter."
+    # No delimiter, only the ID is provided
+    ID="$DASHBOARD"
+    FILENAME="$ID.json"
+    URL="https://grafana.com/api/dashboards/$ID"
+    curl -s "$URL" | jq '.json' > "$TARGET_DIR/$FILENAME"
   fi
 done
 
-echo "Downloaded files:"
+# List the downloaded files
+echo "Downloaded dashboard files:"
 ls -l "$TARGET_DIR"
