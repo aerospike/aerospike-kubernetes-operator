539c22f5dc5a7526025aea1c33e1a06571a635dc
Tanmay Jain <tjain@aerospike.com>
2025-05-05 11:29:33 +0530

    update init image tag

--- api/v1/utils.go
+++ api/v1/utils.go
@@ -69,7 +69,7 @@ const (
 	AerospikeInitContainerNameTagEnvVar            = "AEROSPIKE_KUBERNETES_INIT_NAME_TAG"
 	AerospikeInitContainerDefaultRegistry          = "docker.io"
 	AerospikeInitContainerDefaultRegistryNamespace = "aerospike"
-	AerospikeInitContainerDefaultNameAndTag        = "aerospike-kubernetes-init:2.3.0-dev1"
+	AerospikeInitContainerDefaultNameAndTag        = "aerospike-kubernetes-init:2.3.0-dev2"
 	AerospikeAppLabel                              = "app"
 	AerospikeAppLabelValue                         = "aerospike-cluster"
 	AerospikeCustomResourceLabel                   = "aerospike.com/cr"
