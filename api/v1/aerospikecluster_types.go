47096592bb7b0b5e71427268cfaf788ab06c752f
Abhisek Dwivedi <adwivedi@aerospike.com>
2025-06-20 19:44:08 +0530

    Update release tag to 4.1.0-preview

--- api/v1/aerospikecluster_types.go
+++ api/v1/aerospikecluster_types.go
@@ -1211,7 +1211,7 @@ type AerospikePodStatus struct { //nolint:govet // for readability
 
 // AerospikeCluster is the schema for the AerospikeCluster API
 // +operator-sdk:csv:customresourcedefinitions:displayName="Aerospike Cluster",resources={{Service, v1},{Pod,v1},{StatefulSet,v1}}
-// +kubebuilder:metadata:annotations="aerospike-kubernetes-operator/version=4.0.2"
+// +kubebuilder:metadata:annotations="aerospike-kubernetes-operator/version=4.1.0-preview"
 //
 //nolint:lll // for readability
 type AerospikeCluster struct { //nolint:govet // for readability
