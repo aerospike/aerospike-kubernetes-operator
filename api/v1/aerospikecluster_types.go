c764ddf6f5af4909ea2f3866d4bd38361a47efd4
Tanmay Jain <tjain@aerospike.com>
2025-08-13 12:59:13 +0530

    Updating server, AKO and init image tags.

--- api/v1/aerospikecluster_types.go
+++ api/v1/aerospikecluster_types.go
@@ -1233,7 +1233,7 @@ type AerospikePodStatus struct { //nolint:govet // for readability
 
 // AerospikeCluster is the schema for the AerospikeCluster API
 // +operator-sdk:csv:customresourcedefinitions:displayName="Aerospike Cluster",resources={{Service, v1},{Pod,v1},{StatefulSet,v1}}
-// +kubebuilder:metadata:annotations="aerospike-kubernetes-operator/version=4.1.0-preview"
+// +kubebuilder:metadata:annotations="aerospike-kubernetes-operator/version=4.1.0"
 //
 //nolint:lll // for readability
 type AerospikeCluster struct { //nolint:govet // for readability
