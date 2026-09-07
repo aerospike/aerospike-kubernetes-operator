85cd61d9c2ff2c76549d48968a043ba3905cb8c9
Siddhant N Trivedi <sidntrivedi012@gmail.com>
2026-09-07 13:28:49 +0530

    fix(ci): fix golangci-lint

--- api/v1beta1/groupversion_info.go
+++ api/v1beta1/groupversion_info.go
@@ -20,8 +20,9 @@ limitations under the License.
 package v1beta1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
 var (
@@ -29,8 +30,28 @@ var (
 	GroupVersion = schema.GroupVersion{Group: "asdb.aerospike.com", Version: "v1beta1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	SchemeBuilder = &schemeBuilder{GroupVersion: GroupVersion}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
 )
+
+// schemeBuilder is a minimal, dependency-free replacement for the deprecated
+// sigs.k8s.io/controller-runtime/pkg/scheme.Builder, kept local so this api
+// package doesn't depend on controller-runtime.
+type schemeBuilder struct {
+	GroupVersion schema.GroupVersion
+	runtime.SchemeBuilder
+}
+
+// Register adds one or more objects to the SchemeBuilder so they can be added to a Scheme.
+func (bld *schemeBuilder) Register(object ...runtime.Object) *schemeBuilder {
+	bld.SchemeBuilder.Register(func(scheme *runtime.Scheme) error {
+		scheme.AddKnownTypes(bld.GroupVersion, object...)
+		metav1.AddToGroupVersion(scheme, bld.GroupVersion)
+
+		return nil
+	})
+
+	return bld
+}
